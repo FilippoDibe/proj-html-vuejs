9b9357c2166faf9b06fc0331be2dfbb4beeca3cd
SergioBuzi <sergio.buzi97@gmail.com>
2024-01-17 12:11:01 +0100

    style: the Company section in AppHome

--- src/components/pages/AppHome.vue
+++ src/components/pages/AppHome.vue
@@ -117,19 +117,53 @@ export default{
 </div>
 
 <!-- sezione  the company-->
+
 <div id="container-company">
+
     <div id="box-company-text">
-        <div id="title-company">
-            <h1>Excellence in 
-                <span class="title-decor" style="z-index: 1;">Services </span>  
-            </h1>
-            <div class="color-block"></div>
+
+        <div id="box-margin-company-text">
+
+            <div id="title-company">
+                <h1 style="font-size: 48px; font-weight: 700;">The
+                    <span class="title-decor" >Company</span>
+                </h1>
+                <div class="color-block"></div>
+            </div>
+            <p>For 12 years we have been providing audit and warranty, financial advice, risk advice, taxes and related services to select clients.</p>
+            
+            <div id="company-row">
+
+                <div class="card-company">
+                    <h3><i class="fa-solid fa-hourglass-half"></i> Tradition</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                </div>
+
+                <div class="card-company">
+                    <h3><i class="fa-solid fa-lock"></i> Securety</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                </div>
+
+                <div class="card-company">
+                    <h3><i class="fa-solid fa-apple-whole"></i> Certificate</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                </div>
+
+                <div class="card-company">
+                    <h3><i class="fa-solid fa-user"></i> Expertise</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                </div>
+                
+            </div>
+
         </div>
-    </div>
-
-    <div id="box-company-image">
+        
 
     </div>
+
+    
+
+    <div id="box-company-image"></div>
 </div>
 
 </template>
@@ -264,26 +298,44 @@ export default{
     #box-company-text{
         width: 50%;
         height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
 
-        #title-company{
-        align-self: flex-start;
-        margin-bottom: 30px;
-        position: relative;
-        color: white;
+        #box-margin-company-text{
+            width: 70%;
+            margin: auto;
 
-        .title-decor{
-            position: relative;
+            #title-company{
+                align-self: flex-start;
+                position: relative;
+                color: white;
+
+                .title-decor{
+                    position: relative;
+                    z-index: 1;
+                }
+                .color-block{
+                    width: 220px;
+                    height: 25px;
+                    background-color: #0b4653;
+                    border-radius: 3px;
+                    position: absolute; 
+                    left: 100px;
+                    top: 25px;
+                }
+            }
+            
         }
-        .color-block{
-            width: 190px;
-            height: 25px;
-            background-color: #0b4653;
-            position: absolute; 
-            left: 320px;
-            top: 25px;
+
+        p{
+            color: #888c9e;
+            margin-bottom: 30px;
         }
     }
-    }
+
+    
 
     #box-company-image{
         width: 50%;
@@ -295,5 +347,28 @@ export default{
     }
     
 }
+
+
+#company-row{
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+
+    h3{
+        color: white;
+    }
+
+    .fa-solid{
+        color: #048282;
+    }
+
+    .card-company{
+        width: calc(100% / 2);
+        
+    }
+}
+
 
 </style>
