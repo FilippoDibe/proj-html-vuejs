1e32f1de8ad9b7bb7563aef2cd31b8ee31d63dbb
SergioBuzi <sergio.buzi97@gmail.com>
2024-01-17 17:01:22 +0100

    config: AppProjects

--- src/components/pages/home/AppServicesHome.vue
+++ src/components/pages/home/AppServicesHome.vue
@@ -1,172 +1,175 @@
 <script>
+
 export default {
-  name: "AppServicesHome",
-};
+    name: 'AppServicesHome',
+}
 </script>
 
 <template>
-  <!-- sezione con la lista dei servizi -->
-  <div id="container-services">
+
+<!-- sezione con la lista dei servizi -->
+<div id="container-services">
+
     <div id="box-services">
-      <div id="title">
-        <h1>
-          Excellence in
-          <span class="title-decor" style="color: #048282; z-index: 1"
-            >Services
-          </span>
-        </h1>
-        <div class="color-block"></div>
-      </div>
 
-      <div id="p-and-button">
-        <p>
-          We are leaders in providing consultancy services with a set of
-          cutting-edge technologies and a team of experienced and renowned
-          professionals. These are some options that you can hire.
-        </p>
-        <a href="" id="btn">SEE ALL</a>
-      </div>
-
-      <!-- sezione con le cards services -->
-      <div id="row-services">
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-network-wired" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
-
-          <h3>Audit & Assurance</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+        <div id="title">
+            <h1>Excellence in 
+                <span class="title-decor" style="color: #048282;z-index: 1;">Services </span>  
+            </h1>
+            <div class="color-block"></div>
         </div>
 
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-briefcase" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
-
-          <h3>Financial Advisory</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+        <div id="p-and-button">
+            <p>We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowned professionals. These are some options that you can hire.</p>
+            <a href="" id="btn">SEE ALL</a>
         </div>
+        
+        <!-- sezione con le cards services -->
+        <div id="row-services">
 
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-chart-simple" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
+            <div class="card">
 
-          <h3>Analytics and M&A</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-network-wired" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
 
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-plane" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
+                <h3>Audit  & Assurance</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
 
-          <h3>Middle Marketing</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
+            </div>
 
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-globe" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
+            <div class="card">
 
-          <h3>Legal Consulting</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-briefcase" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
 
-        <div class="card">
-          <div class="icon-and-btn">
-            <i class="fa-solid fa-folder-open" style="font-size: 50px"></i>
-            <a href="" style="font-size: 30px"
-              ><i class="fa-solid fa-arrow-right"></i
-            ></a>
-          </div>
+                <h3>Financial Advisory</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
 
-          <h3>Audit & Assurance</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
-        </div>
-      </div>
+            </div>
+
+            <div class="card">
+
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-chart-simple" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+
+                <h3>Analytics and M&A</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+
+            </div>
+            
+            <div class="card">
+
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-plane" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+
+                <h3>Middle Marketing</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+
+            </div>
+
+            <div class="card">
+
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-globe" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+
+                <h3>Legal Consulting</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+
+            </div>
+
+            <div class="card">
+
+                <div class="icon-and-btn">
+                    <i class="fa-solid fa-folder-open" style="font-size: 50px;"></i>
+                    <a href="" style="font-size: 30px;"><i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+
+                <h3>Audit  & Assurance</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+
+            </div>
+            
+        </div>      
+
     </div>
-  </div>
+
+</div>
+
 </template>
 
 <style lang="scss" scoped>
-#container-services {
-  width: 100%;
-  min-height: 910px;
-  background-color: #e3e3e6;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+#container-services{
+    width: 100%;
+    min-height: 910px;
+    background-color: #e3e3e6; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-#box-services {
-  width: 60%;
-  max-width: 80%;
-  margin: auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  position: relative;
-
-  #title {
-    align-self: flex-start;
-    margin-bottom: 30px;
-    position: relative;
-
-    .title-decor {
-      position: relative;
-    }
-    .color-block {
-      width: 190px;
-      height: 25px;
-      background-color: #c1dadf;
-      position: absolute;
-      left: 320px;
-      top: 25px;
-    }
-  }
-
-  h1 {
-    font-size: 48px;
-    font-weight: 600;
-  }
-
-  P {
-    width: 80%;
-    color: #888c9e;
-  }
-
-  #p-and-button {
+#box-services{
+    width: 60%;
+    max-width: 80%;
+    margin: auto;
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    flex-direction: column;
+    position: relative;
 
-    #btn {
-      text-decoration: none;
-      background-color: #048282;
-      color: white;
-      padding: 10px 25px;
-      border-radius: 5px;
+    #title{
+        align-self: flex-start;
+        margin-bottom: 30px;
+        position: relative;
+
+        .title-decor{
+            position: relative;
+        }
+        .color-block{
+            width: 190px;
+            height: 25px;
+            background-color: #c1dadf;
+            position: absolute; 
+            left: 320px;
+            top: 25px;
+        }
     }
-  }
+
+    h1{
+        font-size: 48px;
+        font-weight: 600;
+    }
+
+    P{
+        width: 80%;
+        color:  #888c9e;
+    }
+
+    #p-and-button{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        #btn{
+            text-decoration: none;
+            background-color: #048282;
+            color: white;
+            padding: 10px 25px;
+            border-radius: 5px;
+        }
+        
+    }
 }
 
 #row-services {
@@ -189,26 +192,27 @@ export default {
   border-radius: 3px;
   padding: 35px;
   transition: all 0.5s;
+  cursor: pointer;
 
-  h3 {
+  h3{
     font-size: 20px;
     font-weight: 600;
     margin-bottom: 20px;
   }
 
-  .icon-and-btn {
+  .icon-and-btn{
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 20px;
 
-    .fa-solid {
-      color: #048282;
+    .fa-solid{
+        color: #048282;
     }
   }
 }
 
-.card:hover {
-  transform: translateY(-20px);
+.card:hover{
+    transform: translateY(-20px);
 }
 </style>
