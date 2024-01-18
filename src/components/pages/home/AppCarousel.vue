895e64876a39bfc7ce7c41958814ccf11db3bb55
jacopobiagi <jacopo.biagi02@gmail.com>
2024-01-18 21:49:52 +0100

    config: insert carousel

---
+++ src/components/pages/home/AppCarousel.vue
@@ -0,0 +1,108 @@
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Parallax, Pagination, Navigation } from 'swiper/modules';
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Parallax, Pagination, Navigation],
+        };
+    },
+};
+</script>
+
+<template>
+    <swiper :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+    }" :speed="600" :parallax="true" :pagination="{
+    clickable: true,
+}" :navigation="true" :modules="modules" class="mySwiper">
+        <div slot="container-start" class="parallax-bg" :style="{
+            'background-image':
+                'url(https://nexgen.codings.dev/wp-content/uploads/2021/03/bg-parallax.png)',
+        }" data-swiper-parallax="-23%"></div>
+        <swiper-slide>
+            <div class="title" data-swiper-parallax="-300">Slide 1</div>
+            <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+            <div class="text" data-swiper-parallax="-100">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                    dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                </p>
+            </div>
+        </swiper-slide><swiper-slide>
+            <div class="title" data-swiper-parallax="-300">Slide 2</div>
+            <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+            <div class="text" data-swiper-parallax="-100">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                    dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                </p>
+            </div>
+        </swiper-slide><swiper-slide>
+            <div class="title" data-swiper-parallax="-300">Slide 3</div>
+            <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
+            <div class="text" data-swiper-parallax="-100">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                    dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                </p>
+            </div>
+        </swiper-slide>
+    </swiper>
+</template>
+
+<style scoped>
+.swiper {
+    height: 100vh;
+    background: #ffffff;
+}
+
+.swiper-slide {
+    font-size: 18px;
+    color: #fff;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 40px 60px;
+}
+
+.parallax-bg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 200%;
+    height: 100%;
+    -webkit-background-size: cover;
+    background-position: center;
+    background-size: cover;
+}
+
+.swiper-slide .title {
+    font-size: 41px;
+    font-weight: 300;
+}
+
+.swiper-slide .subtitle {
+    font-size: 21px;
+}
+
+.swiper-slide .text {
+    font-size: 14px;
+    max-width: 400px;
+    line-height: 1.3;
+}
+</style>
--- src/components/pages/home/AppHome.vue
+++ src/components/pages/home/AppHome.vue
@@ -1,127 +1,43 @@
 <script>
 
-// Import Swiper styles
-import 'swiper/css';
-
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-// import required modules
-import { Parallax, Pagination, Navigation } from 'swiper/modules';
-
-
-
 import AppProjects from '../home/AppProjects.vue';
-import AppCompany from '../home/AppCompany.vue'
-import AppServicesHome from '../home/AppServicesHome.vue'
-import AppResults from '../home/AppResults.vue'
+import AppCompany from '../home/AppCompany.vue';
+import AppServicesHome from '../home/AppServicesHome.vue';
+import AppResults from '../home/AppResults.vue';
 import AppContacts from './AppContacts.vue';
+import AppCarousel from './AppCarousel.vue';
 
-export default{
-    name: 'AppHome',
-    components: { 
-        AppServicesHome,
-        AppCompany,
-        AppProjects,
-        AppResults,
-        AppContacts,
-    },
-    setup() {
-    return {
-      modules: [Parallax, Pagination, Navigation],
-    };
-  },
-};
+export default {
+  name: 'AppHome',
+  components: {
+    AppServicesHome,
+    AppCompany,
+    AppProjects,
+    AppResults,
+    AppContacts,
+    AppCarousel,
+  }
+}
 </script>
 
 <template>
-
-<!-- sezione con il carosello -->
-<swiper
-    :style="{
-      '--swiper-navigation-color': '#fff',
-      '--swiper-pagination-color': '#fff',
-    }"
-    :speed="600"
-    :parallax="true"
-    :pagination="{
-      clickable: true,
-    }"
-    :navigation="true"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <div
-      slot="container-start"
-      class="parallax-bg"
-      :style="{
-        'background-image':
-          'url(https://swiperjs.com/demos/images/nature-1.jpg)',
-      }"
-      data-swiper-parallax="-23%"
-    ></div>
-    <swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 1</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-          laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-          Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-          Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper
-          velit. Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut
-          libero. Aenean feugiat non eros quis feugiat.
-        </p>
-      </div> </swiper-slide
-    ><swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 2</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-          laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-          Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-          Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper
-          velit. Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut
-          libero. Aenean feugiat non eros quis feugiat.
-        </p>
-      </div> </swiper-slide
-    ><swiper-slide>
-      <div class="title" data-swiper-parallax="-300">Slide 3</div>
-      <div class="subtitle" data-swiper-parallax="-200">Subtitle</div>
-      <div class="text" data-swiper-parallax="-100">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-          laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-          Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-          Aliquam hendrerit lorem at elit facilisis rutrum. Ut at ullamcorper
-          velit. Nulla ligula nisi, imperdiet ut lacinia nec, tincidunt ut
-          libero. Aenean feugiat non eros quis feugiat.
-        </p>
-      </div>
-    </swiper-slide>
-  </swiper>
+  <!-- sezione con il carosello -->
+  <AppCarousel />
 
 
-<!-- sezione con i componenti  -->
-<AppServicesHome />
-<AppCompany />
-<AppProjects />
-<AppResults />
-<AppContacts />
-
+  <!-- sezione con i componenti  -->
+  <AppServicesHome />
+  <AppCompany />
+  <AppProjects />
+  <AppResults />
+  <AppContacts />
 </template>
 
 <style lang="scss" scoped>
 // stili del carosello
-#jumbotron{
-    background-color: grey;
-    width: 100%;
-    height: 100vh;
+#jumbotron {
+  background-color: grey;
+  width: 100%;
+  height: 100vh;
 }
-
-
 </style>
